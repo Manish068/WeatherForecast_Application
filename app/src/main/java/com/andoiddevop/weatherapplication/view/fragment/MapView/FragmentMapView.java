92b919aaba4d98cb28a3078f43620578656f4f73
Manish Pandit <mkmk6499@gmail.com>
2020-11-10 17:57:42 +0530

    Setting Pages done

--- app/src/main/java/com/andoiddevop/weatherapplication/view/fragment/MapView/FragmentMapView.java
+++ app/src/main/java/com/andoiddevop/weatherapplication/view/fragment/MapView/FragmentMapView.java
@@ -52,7 +52,7 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class FragmentMapView extends BaseFragment implements OnMapReadyCallback ,GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMapLongClickListener{
+public class FragmentMapView extends BaseFragment  {
 
     private static final int REQUEST_LOCATION = 123;
     private final float DEFAULT_ZOOM = 15;
@@ -74,189 +74,12 @@ public class FragmentMapView extends BaseFragment implements OnMapReadyCallback 
         init(view);
 
 
-
         return view;
     }
 
     private void init(View view) {
 
-        mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.mapView);
-        mapFragment.getMapAsync( this);
-        mapView = mapFragment.getView();
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
-
-
-
 
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-        mMap = googleMap;
-        try {
-            // Customise the styling of the base map using a JSON object defined
-            // in a raw resource file.
-            boolean success = googleMap.setMapStyle(
-                    MapStyleOptions.loadRawResourceStyle(
-                            getActivity(), R.raw.mapstyle_json));
-
-            if (!success) {
-                Log.e("MAPSTYLE", "Style parsing failed.");
-            }
-        } catch (Resources.NotFoundException e) {
-            Log.e("MAPSTYLE", "Can't find style. Error: ", e);
-        }
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        } else {
-            mMap.setMyLocationEnabled(true);
-            mMap.getUiSettings().setMyLocationButtonEnabled(true);
-            if (mapView != null && mapView.findViewById(Integer.parseInt("1")) != null) {
-                View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
-                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
-                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
-                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0);
-                layoutParams.setMargins(0, 1400, 40, 0);
-                locationButton.setBackgroundColor(Color.YELLOW);
-            }
-        }
-
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
-
-        SettingsClient settingsClient = LocationServices.getSettingsClient(getActivity());
-        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(builder.build());
-
-        task.addOnSuccessListener(getActivity(), locationSettingsResponse -> {
-            getDeviceLocation();
-        });
-
-        task.addOnFailureListener(getActivity(), e -> {
-            if (e instanceof ResolvableApiException) {
-                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
-                try {
-                    resolvableApiException.startResolutionForResult(getActivity(), 51);
-                } catch (IntentSender.SendIntentException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-
-        mMap.setOnMapLongClickListener(this);
-        mMap.setOnMyLocationButtonClickListener(this);
-
-
-    }
-
-    @SuppressLint("MissingPermission")
-    @Override
-    public boolean onMyLocationButtonClick() {
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        LocationServices.getFusedLocationProviderClient(getActivity())
-                .requestLocationUpdates(locationRequest, new LocationCallback() {
-                    @Override
-                    public void onLocationResult(final LocationResult locationResult) {
-                        super.onLocationResult(locationResult);
-                        LocationServices.getFusedLocationProviderClient(getActivity())
-                                .removeLocationUpdates(this);
-                        if (locationResult != null && locationResult.getLocations().size() > 0) {
-
-                            mapFragment.getMapAsync(googleMap -> {
-
-                                int latestLocationIndex = locationResult.getLocations().size() - 1;
-                                double latitude = locationResult.getLocations().get(latestLocationIndex).getLatitude();
-                                double longitude = locationResult.getLocations().get(latestLocationIndex).getLongitude();
-
-
-                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 17));
-                                Location location = new Location("providerNA");
-                                location.setLatitude(latitude);
-                                location.setLongitude(longitude);
-                            });
-
-                        }
-
-                    }
-                }, Looper.getMainLooper());
-        return false;
-
-    }
-
-    @Override
-    public void onMapLongClick(LatLng latLng) {
-
-        Location location = new Location("providerNA");
-        location.setLatitude(latLng.latitude);
-        location.setLongitude(latLng.longitude);
-
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_LOCATION) {
-
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                System.out.println("Location permissions granted, starting location");
-
-
-            }
-        }
-    }
-
-
-    @SuppressLint("MissingPermission")
-    private void getDeviceLocation() {
-        fusedLocationProviderClient.getLastLocation()
-                .addOnCompleteListener(new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful()) {
-                            mLastKnownLocation = task.getResult();
-                            if (mLastKnownLocation != null) {
-                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
-                                Location location = new Location("providerNA");
-                                location.setLatitude(mLastKnownLocation.getLatitude());
-                                location.setLongitude(mLastKnownLocation.getLongitude());
-                            } else {
-                                LocationRequest locationRequest = LocationRequest.create();
-                                locationRequest.setInterval(10000);
-                                locationRequest.setFastestInterval(5000);
-                                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                                locationCallback = new LocationCallback() {
-                                    @Override
-                                    public void onLocationResult(LocationResult locationResult) {
-                                        super.onLocationResult(locationResult);
-                                        if (locationResult == null) {
-                                            return;
-                                        }
-                                        mLastKnownLocation = locationResult.getLastLocation();
-                                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
-                                        Location location = new Location("providerNA");
-                                        location.setLatitude(mLastKnownLocation.getLatitude());
-                                        location.setLongitude(mLastKnownLocation.getLongitude());
-                                        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
-
-                                    }
-                                };
-                                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
-                            }
-                        } else {
-                            Toast.makeText(getActivity(), "unable to get last location", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
 }
